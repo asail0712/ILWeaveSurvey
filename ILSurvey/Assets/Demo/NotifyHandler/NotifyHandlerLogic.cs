9f0b37089cfa2255888e7adf2e0fc98eb9008d71
asail0712 <asail0712@gmail.com>
2025-11-24 15:26:45 +0800

    [Add]增加新範例 去Demo NotifyHandler

---
+++ ILSurvey/Assets/Demo/NotifyHandler/NotifyHandlerLogic.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+using XPlan;
+using XPlan.Observe;
+
+namespace WeaveTarget
+{
+    public class DemoMessage : MessageBase
+    {
+        public string demoStr;
+
+        public DemoMessage(string demoStr)
+        {
+            this.demoStr = demoStr;
+        }
+    }
+
+    public class NotifyHandlerLogic : LogicComponent
+    {
+        [NotifyHandler(typeof(DemoMessage))]
+        private void ShowMessage(DemoMessage msg)
+        {
+            Debug.Log(msg.demoStr);
+        }
+    }
+}
---
+++ ILSurvey/Assets/Demo/NotifyHandler/NotifyHandlerSystem.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+using XPlan;
+
+namespace WeaveTarget
+{
+    public class NotifyHandlerSystem : SystemBase
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        protected override void OnInitialLogic()
+        {
+            // for override
+            RegisterLogic(new NotifyHandlerLogic());
+        }
+
+        [ContextMenu("SendMsg XDDDDDDD")]
+        private void SendMsg()
+        {
+            DemoMessage msg = new DemoMessage("XDDDDDDD");
+            msg.Send();
+        }
+    }
+}
